143165fc832c82d5b248157c6ce8194a593eeea1
Rana Uzair <ranauzair686.com>
2024-12-31 23:59:34 +0500

    orderservices model and warehouse model in inventory services

--- Backend/EcommerceInventory/InventoryServices/models.py
+++ Backend/EcommerceInventory/InventoryServices/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from UserServices.models import Users
 # Create your models here.
+
+
+class Warehouse(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50 , null=True , blank=True)
+    address = models.TextField()
+    city = models.CharField(max_length=50 , null=True , blank=True)
+    state = models.CharField(max_length=50 , null=True , blank=True)
+    country = models.CharField(max_length=50 , null=True , blank=True)
+    pincode = models.CharField(max_length=50 , null=True , blank=True)
+    warehouse_manger = models.ForeignKey(Users, on_delete=models.CASCADE , null=True , blank=True , related_name='warehouse_manger')
+    phone = models.CharField(max_length=50 , null=True , blank=True)
+    email = models.EmailField(null=True , blank=True)
+    status = models.CharField(max_length=50 , null=True , blank=True , choices=[('ACTIVE','ACTIVE'),('INACTIVE','INACTIVE')])
+    size = models.CharField(max_length=50 , null=True , blank=True , choices=[('SMALL','SMALL'),('MEDIUM','MEDIUM'),('LARGE','LARGE')])
+    capacity = models.CharField(max_length=50 , null=True , blank=True , choices=[('SMALL','SMALL'),('MEDIUM','MEDIUM'),('LARGE','LARGE')])
+    warehouse_type = models.CharField(max_length=50 , null=True , blank=True , choices=[('OWNED','OWNED'),('LEASED','LEASED')])
+    additional_details = models.JSONField(null=True , blank=True)
+    domain_user_id = models.ForeignKey('UserServices.Users', on_delete=models.CASCADE , null=True , blank=True , related_name='domain_user_id')
+    added_by_user_id = models.ForeignKey('UserServices.Users', on_delete=models.CASCADE , null=True , blank=True , related_name='added_by_user_id')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- Backend/EcommerceInventory/OrderServices/models.py
+++ Backend/EcommerceInventory/OrderServices/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from InventoryServices.models import Warehouse
+from UserServices.models import Users
 
 # Create your models here.
+
+
+class PurchaseOrder(models.Model):
+    id = models.AutoField(primary_key=True)
+    warehouse_id = models.ForeignKey(Warehouse , on_delete=models.CASCADE ,related_name='warehouse_id')
+    supplier_id = models.ForeignKey(Users , on_delete=models.CASCADE , related_name='supplier_id')
+    last_updateb_by_user_id = models.ForeignKey(Users , on_delete=models.CASCADE , related_name='last_updateb_by_user_id')
+    po_code = models.CharField(max_length=50 , null=True , blank=True)
+    po_date = models.DateField(null=True , blank=True)
+    expected_delivery_date = models.DateField(null=True , blank=True)
+    payment_terms = models.CharField(max_length=50 , null=True , blank=True , choices=[('credit','credit'),('cash','cash'),('cheque','cheque'),('online','online')])
+    payment_status = models.CharField(max_length=50 , null=True , blank=True , choices=[('paid','paid'),('unpaid','unpaid'),('partial paid','partial paid') , ('advance','advance'), ('cancelled','cancelled')])
+    total_amount = models.FloatField(null=True , blank=True)
+    paid_amount = models.FloatField(null=True , blank=True)
+    due_amount = models.FloatField(null=True , blank=True)
+    discount_amount = models.FloatField(null=True , blank=True)
+    discount_type = models.CharField(max_length=50 , null=True , blank=True , choices=[('percentage','percentage'),('fixed','fixed')])
+    shipping_amount = models.FloatField(null=True , blank=True)
+    shipping_type = models.CharField(max_length=50 , null=True , blank=True , choices=[('free','free'),('paid','paid')])
+    shipping_tax = models.FloatField(null=True , blank=True)
+    status = models.CharField(max_length=50 , null=True , blank=True , choices=[('draft','draft'),('confirmed','confirmed'),('shipped','shipped'),('delivered','delivered'),('cancelled','cancelled')])
